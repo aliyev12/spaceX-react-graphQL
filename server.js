e36a87adea8fee0bba2c157b40c06934d040bc02
Abdul Aliyev <aliyev_abdul@bah.com>
2019-07-30 16:21:43 -0400

    Add Launch and LaunchItem components

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const port = process.env.PORT || 5000;
 const schema = require("./schema.js");
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
+const cors = require('cors');
 
 const app = express();
+
+// Allow cross origin
+app.use(cors());
 
 app.use(
   "/graphql",
